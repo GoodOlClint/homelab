07a62dae4fea01aab0315af990b08614e723262d
Clint Branham <goodolclint@gmail.com>
2025-06-12 18:44:10 -0500

    move pci.tf into infrastructure, update with new IOMMU group

--- terraform/infrastructure/pci.tf
+++ terraform/infrastructure/pci.tf
@@ -7,7 +7,7 @@ resource "proxmox_virtual_environment_hardware_mapping_pci" "nvidia_gpu" {
       comment = "This is a device specific comment"
       id      = "10de:1eb0"
       # This is an optional attribute, but causes a mapping to be incomplete when not defined.
-      iommu_group = 17
+      iommu_group = 20
       node        = var.virtual_environment_node
       path        = "0000:01:00.0"
       # This is an optional attribute, but causes a mapping to be incomplete when not defined.
